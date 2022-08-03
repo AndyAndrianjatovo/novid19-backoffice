b06a15732f370beaecf47a09018d6f6208a05180
Andy Andrianjatovo <aandrianjatovoniaina@afiassurances.fr>
2022-08-03 14:05:09 +0200

    prod

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontoffice'));
+app.use(express.static(__dirname + '/dist/backoffice'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/frontoffice/index.html'));
+res.sendFile(path.join(__dirname+'/dist/backoffice/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
